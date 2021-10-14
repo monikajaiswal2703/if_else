da8665f9522bd5c2eae5b0a193c6b7a0cf818de8
monikajaiswal2703 <92169645+monikajaiswal2703@users.noreply.github.com>
2021-10-14 14:27:03 +0530

    Add files via upload

---
+++ facebook if.py
@@ -0,0 +1,63 @@
+# language= input("enter the language:")
+# if language == "hindi" or language=="english":
+#     print(language)
+#     num=int (input("enter the phone number:"))
+#     if num>=1000000000 and num<=10000000000:
+#         print("next")
+#         password =  input("enter the password:")
+#         if password>="letter"and"other symbol" and "num":
+#             print("submit")
+#             login=input("press the login button:")
+#             if login=="yes":
+#                 print(login)
+#                 account= input("creat an account")
+#                 if account=="ok" or account=="yes":
+#                     print(account)
+#                     name=input("enter your first name")
+#                     if name==name:
+#                         print("next")
+#                         surname= input("enter your surname")
+#                         if surname==surname:
+#                             print("next")
+#                             num= int (input("enter the phone number"))
+#                             if num>=1000000000 and num<=10000000000:
+#                                 print("next")
+#                                 email= input("enter your email address")
+#                                 if email and"@gmail.com" in email:
+#                                     print("next")
+#                                     password= input("enter new password")
+#                                     if  password>="letter"and"other symbol" and"num":
+#                                         print("next")
+#                                         gender= input("enter the gender")
+#                                         if gender== "male" or gender=="female" or gender=="costume":
+#                                             print(gender)
+#                                             singup= input("enter yes or no")
+#                                             if singup=="yes" and "no":
+#                                                 print("singup")
+#                                             else:
+#                                                 print("not ok")    
+#                                         else:
+#                                             print("error")
+#                                     else:
+#                                         print("invalid")
+#                                 else:
+#                                     print("error")
+#                             else:
+#                                 print("invali")
+#                         else:
+#                             print("wrong")
+#                     else:
+#                         print("nothing")
+#                 else:
+#                     print("error")
+#             else:
+#                 print("error")
+#         else:
+#             print("error")
+#     else:
+#         print("nothing")
+# else:
+#     print("error")                                                                                        
+
+
+
