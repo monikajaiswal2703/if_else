c379386ad4566c8767d088039878a2ef67b55fea
monikajaiswal2703 <92169645+monikajaiswal2703@users.noreply.github.com>
2021-10-23 11:09:20 +0530

    Add files via upload

---
+++ five user input if.py
@@ -0,0 +1,9 @@
+# num = int(input("enter the number"))
+# num1 = int(input("enter the number"))
+# num2 = int(input("enter the number"))
+# num3 = int(input("enter the number"))
+# num4 = int(input("enter the number"))
+# if ("num>num1> and num2>num3 and <num4"):
+#     print("1 and 2nd greater than")
+# else:
+#     print("smaller") 
