59b75b2f52dfc44f5b906402d13662815ef8ce7e
monikajaiswal2703 <92169645+monikajaiswal2703@users.noreply.github.com>
2021-10-23 11:06:35 +0530

    Add files via upload

---
+++ salary if.py
@@ -0,0 +1,10 @@
+# salary = int(input("enter the payment:-"))
+# year_of_service = int (input("enter the number of year:-"))
+# netbones = salary*0.05
+# if year_of_service>5:
+#     print(netbones)
+# else:
+#     print("salary")
+
+
+
